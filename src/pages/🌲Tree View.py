0684eadeb23bfbfb81898fc62359859511757614
Lim Jit <limjit30@gmail.com>
2022-11-01 21:19:14 +0800

    Added ortho lines to graphviz view

--- src/pages/🌲Tree View.py
+++ src/pages/🌲Tree View.py
@@ -6,8 +6,10 @@ import graphviz
 def preorder(node: str):
     if node != 'Untitled':
         for key, val in tree[node].options.items():
+            graph.node(key, shape='box')
             if val != 'Untitled':
                 graph.edge(key, val)
+                graph.node(val, shape='box')
             graph.edge(node, key)
             preorder(val)
 
@@ -16,6 +18,8 @@ if __name__ == '__main__':
     tree = Tree.getTree()
     root = tree.getroot()
     graph = graphviz.Digraph()
+    graph.edge_attr.update(arrowhead='vee', arrowsize='1')
+    graph.graph_attr.update(splines='ortho')
     if root != 'Untitled':
         preorder(root)
         st.graphviz_chart(graph)
