264e9a0d80a75419c5ee4468f42d62186c7d88e3
Lim Jit <limjit30@gmail.com>
2022-11-07 19:19:45 +0800

    Squashed bug with manual data upload process

--- src/pages/Upload Data.py
+++ src/pages/Upload Data.py
@@ -1,9 +1,11 @@
 import streamlit as st 
 from pathlib import Path
 import pickle
+import Tree
 
 
 uploaded = st.file_uploader('Upload .pickle data file')
-if uploaded:
+if uploaded and st.button('Load'):
         t = pickle.load(uploaded)
+        Tree.save(t)
         st.info('Data successfully loaded!')
